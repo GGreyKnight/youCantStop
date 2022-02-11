864b29e658c3da35641c681749bf5204e51e2cf1
GGreyKnight <damiankarwowski9@gmail.com>
2022-02-11 20:12:57 +0100

    Initial commit

---
+++ youCantStopPC/Assets/Scripts/InputController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public string inputSteerAxis = "Horizontal";
+    public string inputThrottleAxis = "Vertical";
+
+    public float ThrottleInput { get; private set; }
+
+    public float SteerInput { get; private set; }
+
+    public bool BreakInput { get; set; }
+
+    public bool QuitInput { get; set; }
+
+
+    void Start()
+    {
+        //goes forward whole time
+        ThrottleInput = 1;
+    }
+
+    void Update()
+    {
+        SteerInput = Input.GetAxis(inputSteerAxis);
+        BreakInput = Input.GetKey(KeyCode.Space);
+        QuitInput = Input.GetKeyDown(KeyCode.Escape);
+        //ThrottleInput = Input.GetAxis(inputThrottleAxis); //car goes forward only forward
+        
+    }
+}
